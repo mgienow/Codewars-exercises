16b6bc51c6578953cb42abe214417f985e8356b7
Michelle Gienow <michellegienow@gmail.com>
2016-12-02 22:30:27 -0500

    grasshopper-summation

---
+++ grasshopper-summation.js
@@ -0,0 +1,17 @@
+/*
+Write a program that finds the summation of every number between 1 and num. The number will always be a positive integer greater than 0.
+
+For example:
+summation(2) -> 3
+1 + 2
+
+summation(8) -> 36
+1 + 2 + 3 + 4 + 5 + 6 + 7 + 8
+*/
+var summation = function (num) {
+  var sum=0;
+  for(var i=0; i<=num; i++){
+    sum=sum + i;
+  }
+  return sum;
+}
