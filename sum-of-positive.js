bb9425125ca9c15637c377eab362db7565abce9b
Michelle Gienow <michellegienow@gmail.com>
2016-12-03 07:49:44 -0500

    sum-of-positive.js

---
+++ sum-of-positive.js
@@ -0,0 +1,22 @@
+/*
+You get an array of numbers, return the sum of all of the positives ones.
+
+Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+Note: array may be empty, in this case return 0.
+*/
+
+function positiveSum(arr) {
+  var sum = 0;
+  if (arr === []) {
+  sum = 0;
+  }
+  else {
+  for(i=0; i<arr.length; i++){
+    if (arr[i] > 0) {
+      sum += arr[i];
+      }
+    }
+  }
+  return sum;
+}
